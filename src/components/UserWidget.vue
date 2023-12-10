81bb3c2c65d053fb4ccad3978ed0a17f849a1ae7
Martin Binder <bndrmrtn@gmail.com>
2023-12-10 22:49:58 +0100

    modal

--- src/components/UserWidget.vue
+++ src/components/UserWidget.vue
@@ -1,5 +1,5 @@
 <template>
-   <nav class="fixed top-0 z-[99] w-full">
+   <nav class="fixed top-0 z-30 w-full">
      <div class="w-full px-3 py-3 fontPoppins bg-widget max-w-md mx-auto md:my-2 md:rounded-xl flex items-center">
        <img
            src="https://mrtn.vip/resources/img/nav.c852416e.jpg"
--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -3,17 +3,28 @@
    <Widget class="!text-left">
      <h3 class="fontFredoka text-3xl">{{$t('My Quizzes')}}</h3>
      <ButtonBluish class="mt-3">{{$t('All')}}</ButtonBluish>
-     <ButtonBluish class="mt-3">{{$t('Create new')}}</ButtonBluish>
+     <ButtonBluish @click="showModal = true" class="mt-3">{{$t('Create new')}}</ButtonBluish>
    </Widget>
     <Widget class="!text-left">
       <h3 class="fontFredoka text-3xl">{{$t('Completed Quizzes')}}</h3>
       <ButtonPinkle class="mt-3">{{$t('All')}}</ButtonPinkle>
     </Widget>
   </AuthLayout>
+
+  <Modal :show="showModal" @cancel="showModal = false">
+    <h3 class="text-3xl fontFredoka">Create a Quiz</h3>
+    <SimpleInput class="my-3" placeholder="Your quiz's name" />
+    <ButtonBluish>Create</ButtonBluish>
+  </Modal>
 </template>
 <script setup lang="ts">
 import AuthLayout from "@/components/layouts/AuthLayout.vue";
 import Widget from "@/components/Widget.vue";
 import ButtonBluish from "@/components/buttons/ButtonBluish.vue";
 import ButtonPinkle from "@/components/buttons/ButtonPinkle.vue";
+import Modal from "@/components/modal/Modal.vue";
+import SimpleInput from "@/components/inputs/SimpleInput.vue";
+import {ref} from "vue";
+
+const showModal = ref(false)
 </script>
